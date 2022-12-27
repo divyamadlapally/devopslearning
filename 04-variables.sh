4adce4714f1d9879ca5d93f306f10edb26b5ef3f
divyamadlapally <divyamadlapally@gmail.com>
2022-12-27 20:39:46 +0530

    added variable

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,9 +2,9 @@
 # What is a variable?
 # A variable is something which holds the value or data which would keep on changing as per situation or scenaario.
 # Typically we are telling the computer/program that uses this variable when running the scripts
-a = 10
-b = 20
-c = 30
+a=10
+b=20
+c=30
 # How to print the variable
 echo a 
 #how to print the value of a
