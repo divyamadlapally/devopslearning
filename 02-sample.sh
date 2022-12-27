c2f73ac43d61d68bbff0af4b8a04f9b89c28ff35
divyamadlapally <divyamadlapally@gmail.com>
2022-12-27 19:37:26 +0530

    added equence characters

---
+++ 02-sample.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Escape Sequence Characters
+# Escape sequence characters will be considered by echo command only if we enable it by using -e option
+#
+echo line1
+echo line2
+echo line3
+# \n : new line
+# \t : tab space
