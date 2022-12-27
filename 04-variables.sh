8a7983e249e838dbb1c25abdb79407286cf80d84
divyamadlapally <divyamadlapally@gmail.com>
2022-12-27 20:38:21 +0530

    added variable

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# What is a variable?
+# A variable is something which holds the value or data which would keep on changing as per situation or scenaario.
+# Typically we are telling the computer/program that uses this variable when running the scripts
+a = 10
+b = 20
+c = 30
+# How to print the variable
+echo a 
+#how to print the value of a
+echo $a 
+# If you dont declare a value and if you try to print it, it is going to considerr the value as null
