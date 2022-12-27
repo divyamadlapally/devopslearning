8ce21c7ef70e9b03da908764661337fe7169e817
divyamadlapally <divyamadlapally@gmail.com>
2022-12-27 20:17:05 +0530

    added colouring

--- 03-sample.sh
+++ 03-sample.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Colouring
 echo -e "\e[35m I am printing in magenta colour \e[0m"
-echo -e "\e[43; [32m I am a devops engineer \e[0m"
+echo -e "\e[43; 32m I am a devops engineer \e[0m"
