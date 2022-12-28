740a02f40681bb2d7dc868453b78ae639205e039
divyamadlapally <divyamadlapally@gmail.com>
2022-12-28 17:47:07 +0530

    dynamic variables

--- 05-dynamicvariables.sh
+++ 05-dynamicvariables.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Always fetch the information dynamically for the variable which keep on changing
-Date="24-12-2022"
-echo "good morning, today's date is $Date"
-#DATE=${date +%F}
-#echo todays date is ${DATE}
-
+DATE="24-12-2022"
+echo "good morning, today's date is $DATE"
