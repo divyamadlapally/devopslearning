57ae158fa12674f96ab8fd49e2aeb0badb0e15d2
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 19:50:05 +0530

    case

--- 10-case.sh
+++ 10-case.sh
@@ -4,13 +4,17 @@ ACTION=$1
 case $ACTION in
     start)
     echo "xyz service is starting"
+    exit 0
     ;;
     stop)
     echo "xyz service is stopping"
+    exit 0
     ;;
     restart)
     echo "xyz service is restarting"
+    exit 0
     ;;
     *)
     echo -e "(\e[34m valid options are start or stop or restart only \e[0m )"
+    exit 1
     esac
