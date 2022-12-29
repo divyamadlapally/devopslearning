cbe7c695ad26911d2d7fe4aa02534d111a6f712c
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 21:03:49 +0530

    if

--- 12-if.sh
+++ 12-if.sh
@@ -18,3 +18,18 @@ else
     exit 1
 
 fi
+
+#note : use == when you are dealing with strings, use -eq when dealing with numbers.
+
+b=10
+if [ "$b" -eq "10" ]; then
+   echo "value is b 10"
+elif [ "$b" -eq "20" ]; then
+   echo "value is b 20"
+elif [ "$b" -eq "30" ]; then
+   echo "value is b 30"
+
+else
+   echo "value is not 10 or 20 or 30"
+   
+fi
