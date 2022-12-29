11a2a93a3f370e0e5d712bf44f88403ce064968e
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 20:41:08 +0530

    if

--- 12-if.sh
+++ 12-if.sh
@@ -6,3 +6,13 @@ if [ "$a"=="ABC" ]; then
     
 
 fi
+
+#If else condition
+if [ "$a"=="ABC"]; then
+    echo -e "\e[34m both are equal \e[0m"
+    exit 0
+else
+    echo -e "\e[34m  both are not equal \e[0m"
+    exit 1
+    
+fi
