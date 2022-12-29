ebf51780aab3b8ac99319c5a811a44d1ccbf43ad
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 18:19:55 +0530

    functions

--- 08-function.sh
+++ 08-function.sh
@@ -15,7 +15,10 @@ stat() {
     echo "number of opened sessions : $(who | wc -l)"
     echo "todays date is: $(date +%F)"
     echo "load average on the system in last 1 minute: $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
-echo -e "(\e[32m______ I am done; function completed_____\e[0m)"
+echo -e "(\e[32m______ I am done; stat function completed_____\e[0m)"
+# calling sample function
+echo -e "(\e[33m ______calling sample function______\e[0m)"
+sample
 }
-#echo cualling stat function
+#echo calling stat function
 stat
