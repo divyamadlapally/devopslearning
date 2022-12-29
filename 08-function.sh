ebc758fb34544c285f8b66c8b5a4aae7479f65db
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 17:29:06 +0530

    functions

--- 08-function.sh
+++ 08-function.sh
@@ -10,3 +10,9 @@ sample() {
 sample
 #calling the function second time
 sample
+
+stat() {
+    echo "number of opened sessions :"
+    echo "todays date is:"
+    echo "load average on the system in last 1 minute:(uptime | awk -F : '{print $4}' | awk -F , '{print $1}')"
+}
