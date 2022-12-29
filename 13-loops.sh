54085b9c14e1bbc9fb119f808ca9fd2efd5b306e
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 21:44:34 +0530

    loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bashfor loop will be used, when the loop is based on inputs
+#
 
 for list in val1 val2 val3 val4 val5 ; do
     echo value is $list
@@ -7,3 +8,11 @@ done
 for courses in devops aws azzure docker jenkins kubernetes ; do
        echo course name is $courses
 done
+
+# while loop : a conditional loop
+
+i=10
+while [ $i -lt 0 ]; do
+  echo "iteration number is $i"
+  i=$(($i-1))
+  done
