23ed5dae59ed6d22d17dc1dd024fc323134cc3b7
divyamadlapally <divyamadlapally@gmail.com>
2022-12-29 21:28:12 +0530

    loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -3,3 +3,7 @@
 for list in val1 val2 val3 val4 val5 ; do
     echo value is $list
 done
+
+for courses in devops aws azzure docker jenkins kubernetes ; do
+       echo course name is $courses
+done
