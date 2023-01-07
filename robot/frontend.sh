3b1f6eed9d7a699cc5bbfc3c5c3c38e79b993e15
divyamadlapally <divyamadlapally@gmail.com>
2023-01-07 20:20:13 +0530

    frontend

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -34,4 +34,3 @@ stat $?
  # send the logs to temporary folder &>> this redirects the std.out and std.error 
 echo -n "downloading the $COMPONENT :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
