bef635121460625027fbdfabe85d89956df57cbd
divyamadlapally <divyamadlapally@gmail.com>
2023-01-07 20:47:27 +0530

    frontend

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -36,8 +36,9 @@ stat $?
 
  # $? tells the exit code of the last command
  # send the logs to temporary folder &>> this redirects the std.out and std.error 
+
 echo -n "downloading the $COMPONENT :"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
 echo -n "clearing the  default content :"
