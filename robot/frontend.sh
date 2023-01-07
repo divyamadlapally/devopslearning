7d0a1da5690dc1351fa20d4f7e4921e79e0f0657
divyamadlapally <divyamadlapally@gmail.com>
2023-01-07 19:27:15 +0530

    frontend

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -13,9 +13,10 @@ echo I am frontend
  fi
 
 # exit 1 means script will be terminated then and there itself
-
+echo installing nginx
  yum install nginx -y   &>> /tmp/frontend.log
  systemctl enable nginx  &>> /tmp/frontend.log
+echo starting nginx
  systemctl start nginx   &>> /tmp/frontend.log
  
  # send the logs to temporary folder &>> this redirects the std.out and std.error 
