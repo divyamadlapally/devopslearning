177de5e192c62048ef15df6892938a93f34c3107
divyamadlapally <divyamadlapally@gmail.com>
2023-01-07 18:53:50 +0530

    frontend

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -5,7 +5,7 @@ echo I am frontend
 # we need run the script as a root user
 # we need to do validation first, we need check whether root use or not $ id  ( $ id -u)if uid is 0 then it is root
 
- UID=$(id -u)
+ UID = $(id -u)
  if [$UID -ne 0] ; then
  echo -e "\e[31m You need to run the script either as a root user or with a sudo privilage \e[0m" 
  exit 1
