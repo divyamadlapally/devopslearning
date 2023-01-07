d9b43a58bf1e2d551d297c32e150eec5a8d64891
divyamadlapally <divyamadlapally@gmail.com>
2023-01-07 19:39:58 +0530

    frontend

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -15,8 +15,13 @@ echo I am frontend
 # exit 1 means script will be terminated then and there itself
 echo installing nginx
  yum install nginx -y   &>> /tmp/frontend.log
+ if [ $? -eq 0] ; then 
+       echo -e "\[36m success \[0m"
+    else
+        echo -e "\e[31m failure \[0m"
+    fi
  systemctl enable nginx  &>> /tmp/frontend.log
 echo starting nginx
  systemctl start nginx   &>> /tmp/frontend.log
- 
+ # $? tells the exit code of the last command
  # send the logs to temporary folder &>> this redirects the std.out and std.error 
