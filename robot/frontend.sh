2f2899d1577e6f603bc6f7f0f2cefcf32bb12ea0
divyamadlapally <divyamadlapally@gmail.com>
2023-01-07 20:33:25 +0530

    frontend

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -36,3 +36,24 @@ stat $?
 echo -n "downloading the $COMPONENT :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
+
+echo -n "clearing the  default content :"
+cd /usr/share/nginx/html
+rm -rf *   &>> /tmp/frontend.log
+stst $?
+
+echo -n "Extracting $COMPONENT :" 
+unzip /tmp/$COMPONENT.zip   &>> /tmp/frontend.log
+stat $?
+
+echo -n "Copying $COMPONENT :"
+mv frontend-main/* .         &>> /tmp/frontend.log
+mv static/* .                &>> /tmp/frontend.log
+rm -rf frontend-main README.md       &>> /tmp/frontend.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stst $?
+
+echo -n " Restarting nginx :"
+systemctl enable nginx  &>> /tmp/frontend.log
+systemctl start nginx   &>> /tmp/frontend.log
+stat $?
