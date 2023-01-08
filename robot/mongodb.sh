429f51e6f62d1eff1233c788d4cd0fa4ea19881f
divyamadlapally <divyamadlapally@gmail.com>
2023-01-08 07:34:47 +0530

    mongodb

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -2,28 +2,12 @@
 set -e
 
 COMPONENT=mongodb
-LOGFILE=/tmp/$COMPONENT.log
 
+source common.sh
 
 # we need run the script as a root user
 # we need to do validation first, we need check whether root use or not $ id  ( $ id -u)if uid is 0 then it is root
-
- ID=$(id -u)
- if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m You need to run the script either as a root user or with a sudo privilage \e[0m" 
-    exit 1
- fi
- # exit 1 means script will be terminated then and there itself
-
-
-stat() 
-{
-    if [ $1 -eq 0 ]; then 
-       echo -e "\e[32m success \e[0m"
-    else
-        echo -e "\e[31m failure \e[0m"
-    fi
-}
+# exit 1 means script will be terminated then and there itself
 
 echo -n "Downloading $COMPONENT : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
