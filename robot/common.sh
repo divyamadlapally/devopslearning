ed72b4d14136c10590b8eea9c577dd35f756c84e
divyamadlapally <divyamadlapally@gmail.com>
2023-01-08 07:33:21 +0530

    common

---
+++ robot/common.sh
@@ -0,0 +1,16 @@
+LOGFILE=/tmp/$COMPONENT.log
+
+ ID=$(id -u)
+ if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m You need to run the script either as a root user or with a sudo privilage \e[0m" 
+    exit 1
+ fi
+
+stat() 
+{
+    if [ $1 -eq 0 ]; then 
+       echo -e "\e[32m success \e[0m"
+    else
+        echo -e "\e[31m failure \e[0m"
+    fi
+}
