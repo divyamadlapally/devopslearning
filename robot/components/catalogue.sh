ad3036a457e34647d631f6664af572864484dd98
divyamadlapally <divyamadlapally@gmail.com>
2023-01-09 07:22:10 +0530

    catalogue

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -5,11 +5,18 @@ COMPONENT=catalogue
 source components/common.sh
 
 echo -n "Configuring the nodejs repo: "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -      &>> $LOGFILE
-yum install nodejs -y   &>> $LOGFILE
+curl -sL https://rpm.nodesource.com/setup_16.x | bash       &>> "${LOGFILE}"
+yum install nodejs -y   &>>  "${LOGFILE}"
 stat $?
 
 echo -n "Creating Application User $APPUSER "
-useradd $APPUSER      &>> $LOGFILE
+useradd $APPUSER      &>>  "${LOGFILE}"
 stat $?
 
+echo -n "Downloading the $COMPONENT : "
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $?
+
+echo -n "Extracting the $COMPONENT : "
+cd /home/$APPUSER
+unzip -o /tmp/catalogue.zip    &>>  "${LOGFILE}"
