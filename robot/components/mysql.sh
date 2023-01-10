347ac478e6404aefb0259c7b9acf5d27ec7de228
divyamadlapally <divyamadlapally@gmail.com>
2023-01-10 20:14:32 +0530

    mysql

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -4,3 +4,17 @@ COMPONENT=mysql
 
 source components/common.sh
 
+echo -n "Configuring $COMPONENT repo : "
+curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo
+stat $?
+
+echo -n " Installing $COMPONENT : "
+yum install mysql-community-server -y   &>> "${LOGFILE}"
+stat $?
+
+echo -n " Starting $COMPONENT : "
+systemctl enable mysqld 
+systemctl start mysqld
+stat $?
+
+
