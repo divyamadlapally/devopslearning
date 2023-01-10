7a83f59cd02ac78380b2c0d5d6c11a1be0b10ec5
divyamadlapally <divyamadlapally@gmail.com>
2023-01-10 18:47:31 +0530

    cart

--- robot/components/common.sh
+++ robot/components/common.sh
@@ -15,3 +15,72 @@ stat()
         echo -e "\e[31m failure \e[0m"
     fi
 }
+
+NODEJS() {
+    echo -n "Configuring nodejs repo : "
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -     &>> "${LOGFILE}"
+    stat $?
+
+    echo  -n "Installing nodejs : "
+    yum install nodejs -y     &>> "${LOGFILE}"
+    stat $?
+
+    CREATE_USER
+
+    DOWNLOAD_AND_EXTRACT
+
+    NPM_INSTALL
+
+    CONFIGURE_SERVICE
+}
+
+CONFIGURE_SERVICE() {
+    echo -n "Configuring the $COMPONENT Service : "
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+    mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+    stat $?
+
+    echo -n "Starting $COMPONENT Service : "
+    systemctl daemon-reload      &>>  "${LOGFILE}"
+    systemctl start $COMPONENT      &>>  "${LOGFILE}"
+    systemctl enable $COMPONENT        &>>  "${LOGFILE}"
+    stat $?
+
+
+    echo -e "\e[32m_____$COMPONENT Configuration is completed______\e[0m"
+
+}
+
+CREATE_USER() {
+    id $APPUSER       &>>  "${LOGFILE}"
+    if [ $? -ne 0 ] ; then
+    echo -n "Creating Application User $APPUSER : "
+    useradd $APPUSER      &>>  "${LOGFILE}"
+    stat $?
+fi
+}
+
+DOWNLOAD_AND_EXTRACT() {
+    echo -n "Downloading the $COMPONENT : "
+    curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+    stat $?
+
+    echo -n "Cleanup and Extracting the $COMPONENT : "
+    rm -rf /home/$APPUSER/$COMPONENT/
+    cd /home/$APPUSER
+    unzip -o /tmp/$COMPONENT.zip    &>>  "${LOGFILE}"
+    stat $?
+
+    echo -n "Changing the ownership to $APPUSER"
+    mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
+    chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+    stat $?
+}
+
+NPM_INSTALL() {
+    echo -n "Installing $COMPONENT Dependencies :"
+    cd $COMPONENT
+    npm install    &>>  "${LOGFILE}"
+    stat $?
+    
+}
