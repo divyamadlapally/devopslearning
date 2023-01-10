55c937ccadeacb2d478ac61a02531756f9e5315d
divyamadlapally <divyamadlapally@gmail.com>
2023-01-10 18:15:00 +0530

    user

--- robot/components/user.sh
+++ robot/components/user.sh
@@ -19,7 +19,7 @@ if [ $? -ne 0 ] ; then
     stat $?
 fi
 echo -n "Downloading the $COMPONENT : "
-curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
 echo -n "Cleanup and Extracting the $COMPONENT : "
