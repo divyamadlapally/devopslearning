5b2ccb1764c990da85fe4aab5714ee96f544ffb1
divyamadlapally <divyamadlapally@gmail.com>
2023-01-10 18:05:13 +0530

    user

--- robot/components/user.sh
+++ robot/components/user.sh
@@ -1,2 +1,55 @@
 #!/bin/bash
-echo I am user
+
+COMPONENT=user
+
+source components/common.sh
+
+echo -n "Configuring nodejs repo : "
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -     &>> "${LOGFILE}"
+stat $?
+
+echo  -n "Installing nodejs : "
+yum install nodejs -y     &>> "${LOGFILE}"
+stat $?
+
+id $APPUSER       &>>  "${LOGFILE}"
+if [ $? -ne 0 ] ; then
+    echo -n "Creating Application User $APPUSER : "
+    useradd $APPUSER      &>>  "${LOGFILE}"
+    stat $?
+fi
+echo -n "Downloading the $COMPONENT : "
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $?
+
+echo -n "Cleanup and Extracting the $COMPONENT : "
+rm -rf /home/$APPUSER/$COMPONENT/
+cd /home/$APPUSER
+unzip -o /tmp/user.zip    &>>  "${LOGFILE}"
+stat $?
+
+echo -n "Changing the ownership to $APPUSER"
+mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
+chown -R $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
+stat $?
+
+echo -n "Installing $COMPONENT Dependencies :"
+cd $COMPONENT
+npm install    &>>  "${LOGFILE}"
+stat $?
+
+echo -n "Configuring the $COMPONENT Service : "
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
+stat $?
+
+echo -n "Starting $COMPONENT Service : "
+systemctl daemon-reload      &>>  "${LOGFILE}"
+systemctl start $COMPONENT      &>>  "${LOGFILE}"
+systemctl enable $COMPONENT        &>>  "${LOGFILE}"
+stat $?
+
+
+echo -e "\e[32m_____$COMPONENT Configuration is completed______\e[0m"
+
+
