f8971a357e72443d052dc621e97a2e3d9f2d6b9e
divyamadlapally <divyamadlapally@gmail.com>
2023-01-10 22:14:23 +0530

    mysql

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -29,9 +29,9 @@ if [ $? -ne 0 ]; then
     stat $?
 fi
 
-echo "show plugins | mysql -uroot -pRoboShop@1 | grep validate_password;"    &>> "${LOGFILE}"
+echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password;   &>> "${LOGFILE}"
 if [ $? -eq 0 ]; then
     echo -n " Uninstalling password validate plugin : "
-    echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password    &>> "${LOGFILE}"
+    echo "show plugins;" |  mysql -uroot -pRoboShop@1 | grep validate_password    &>> "${LOGFILE}"
     stat $?
 fi
